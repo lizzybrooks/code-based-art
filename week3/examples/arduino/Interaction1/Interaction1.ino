60fd5c38e4c4bd4e72e1b262c990f0f553ad2099
lizzybrooks <lizzy.brooks@gmail.com>
2016-10-17 21:20:37 -0700

    Update Interaction1.ino

--- week3/examples/arduino/Interaction1/Interaction1.ino
+++ week3/examples/arduino/Interaction1/Interaction1.ino
@@ -1,6 +1,6 @@
 /*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
+  modified from Blink
+  Turns on an LED while you hold the mouse down-- use this sketch with the corresponding sketch in Processing to.
 
   Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO 
   it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN takes care 
@@ -15,19 +15,46 @@
   
   modified 2 Sep 2016
   by Arturo Guadalupi
+  
+  modified 17 October 2016
+  by Lizzy Brooks
 */
 
+int incomingMessage;      // a variable to read incoming serial data into
 
 // the setup function runs once when you press reset or power the board
 void setup() {
+
+// initialize serial communication:
+  Serial.begin(9600);
+
+    
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
+  
+//see if there's a serial message coming from Processing
+  if (Serial.available()) {
+  
+  //read the most recent message:
+  incomingMessage = Serial.read();
+
+ //if you got 1, turn on the light
+
+ if (incomingMessage == '1') {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
+ // delay(1000);                       // we might not need this delay
+ }
+ else if (incomingMessage == '0' ){
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+ //  delay(1000);                       // wait for a second
+ }
 }
+
+} 
+  
+  
+ 
